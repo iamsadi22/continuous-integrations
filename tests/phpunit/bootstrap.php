c14729eaf4a101baa179da891ca990629cfa851d
sadi1991 <sadi@coderex.co>
2024-06-04 18:56:43 +0600

    update release script

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -22,16 +22,18 @@ if ( false === strpos( getenv( 'WP_PHPUNIT__TESTS_CONFIG' ), '/continuous-integr
 // Composer autoloader must be loaded before WP_PHPUNIT__DIR will be available.
 require_once dirname( __DIR__, 2 ) . '/vendor/autoload.php';
 
+$wp = getenv( 'WP_TESTS_DIR' ) ? getenv( 'WP_TESTS_DIR' ) : '/tmp/wordpress-tests-lib';
+
 // Give access to tests_add_filter() function.
-require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
+require_once $wp . '/includes/functions.php';
 
 // Activate the plugin.
 tests_add_filter(
 	'muplugins_loaded',
 	function() {
-		require dirname( __DIR__, 2 ) . '/the-plugin-name.php';
+		require dirname( __DIR__, 2 ) . '/continuous-integrations.php';
 	}
 );
 
 // Start up the WP testing environment.
-require getenv( 'WP_PHPUNIT__DIR' ) . '/includes/bootstrap.php';
+require $wp . '/includes/bootstrap.php';
