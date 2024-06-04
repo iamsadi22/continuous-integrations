5b2b1ad6f823e1b13475cf7c8671a48131a1ac54
sadi1991 <sadi@coderex.co>
2024-06-04 12:25:37 +0600

    add weekly build script

--- bin/build-plugin-zip.sh
+++ bin/build-plugin-zip.sh
@@ -29,43 +29,6 @@ warning () {
 
 status "💃 Time to build the plugin ZIP file 🕺"
 
-if [ -z "$NO_CHECKS" ]; then
-	# Make sure there are no changes in the working tree. Release builds should be
-	# traceable to a particular commit and reliably reproducible.
-	changed=
-	if ! git diff --exit-code > /dev/null; then
-		changed="file(s) modified"
-	elif ! git diff --cached --exit-code > /dev/null; then
-		changed="file(s) staged"
-	fi
-	if [ ! -z "$changed" ]; then
-		git status
-		error "ERROR: Cannot build plugin zip with dirty working tree. ☝️
-		Commit your changes and try again."
-		exit 1
-	fi
-
-	# Do a dry run of the repository reset. Prompting the user for a list of all
-	# files that will be removed should prevent them from losing important files!
-	status "Resetting the repository to pristine condition. ✨"
-	to_clean=$(git clean -xdf --dry-run)
-	if [ ! -z "$to_clean" ]; then
-		echo $to_clean
-		warning "🚨 About to delete everything above! Is this okay? 🚨"
-		echo -n "[y]es/[N]o: "
-		read answer
-		if [ "$answer" != "${answer#[Yy]}" ]; then
-			# Remove ignored files to reset repository to pristine condition. Previous
-			# test ensures that changed files abort the plugin build.
-			status "Cleaning working directory... 🛀"
-			git clean -xdf
-		else
-			error "Fair enough; aborting. Tidy up your repo and try again. 🙂"
-			exit 1
-		fi
-	fi
-fi
-
 # Run the build.
 status "Installing dependencies... 📦"
 npm cache verify
@@ -84,11 +47,9 @@ zip -r continuous-integrations.zip \
 	languages \
 	views \
 	build \
+	vendor \
 	readme.txt \
 	changelog.txt \
 	README.md
 
-# Reset `gutenberg.php`.
-git checkout gutenberg.php
-
-success "Done. You've built Gutenberg! 🎉 "
+success "Done. You've built Continuous integrations! 🎉 "
